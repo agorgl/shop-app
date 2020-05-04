f676d21ef8d6d2bc16b5b837ef5607b110a5790d
TheArtist <agorglouk@gmail.com>
2020-05-04 22:04:55 +0300

    Formatted generated sources

--- src/main/java/com/samplecorp/Application.java
+++ src/main/java/com/samplecorp/Application.java
@@ -6,12 +6,11 @@ import io.swagger.v3.oas.annotations.info.*;
 
 @OpenAPIDefinition(
     info = @Info(
-            title = "shop-app",
-            version = "0.0"
+        title = "shop-app",
+        version = "0.0"
     )
 )
 public class Application {
-
     public static void main(String[] args) {
         Micronaut.run(Application.class);
     }
